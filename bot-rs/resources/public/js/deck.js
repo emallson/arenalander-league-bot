54046cad8747b31f8e45f1d433354a50e3e1182c
David Smith <emallson@emallson.net>
2020-07-19 12:09:41 -0400

    added link to copy import code on deck page

---
+++ bot-rs/resources/public/js/deck.js
@@ -0,0 +1,12 @@
+function arena_export() {
+    let rows = Array.from(document.querySelectorAll('.deck-sections li'));
+    let result = "Deck\r\n" + rows.map(row => {
+        let count = row.querySelector('.card-count').innerText;
+        let name = row.querySelector('.mtgcard').innerText;
+        return `${count} ${name}`;
+    }).join('\r\n');
+
+    navigator.clipboard.writeText(result)
+             .then(() => document.querySelector('#copy-link').classList.add('copy-success'))
+             .catch(err => alert("Unable to copy to clipboard"));
+}
